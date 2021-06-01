e15bec60b3519cf322b641c6906d44085727ecc9
Szabolcs Szanto <szabolcs.szanto@lynxsolutions.eu>
2021-06-01 17:41:31 +0300

    Task #81083 - Setup:

--- src/wallet.js
+++ src/wallet.js
@@ -64,19 +64,19 @@ const sendTokenToUser = async (sender_private_key, toAddress, amount, gas) => {
          *
          * @type {string}
          */
-        const encodedContractData = await L2LContract.methods.transfer(toAddress, amountInWei).encodeABI();
+        const encodedContractData = await L2LContract.methods.transfer(toAddress, amountInWei.toString()).encodeABI();
 
         /**
          * Estimate gas price for current transfer
          *
          * @type {BigNumber | number}
          */
-        const currentGasEstimation = parseFloat(await L2LContract.methods.transfer(toAddress,amountInWei).estimateGas());
+        const currentGasEstimation = parseFloat(await L2LContract.methods.transfer(toAddress,amountInWei.toString()).estimateGas());
 
         /**
          * Approve the transaction
          */
-        const transactionApproved = await L2LContract.methods.approve(toAddress, amountInWei).call();
+        const transactionApproved = await L2LContract.methods.approve(toAddress, amountInWei.toString()).call();
 
         /**
          * Defines the raw transaction object
@@ -111,7 +111,7 @@ const sendTokenToUser = async (sender_private_key, toAddress, amount, gas) => {
         if(currentGasEstimation > gas) {
             response.error.gas = `Gas too low.You have to set minim gas to: ${currentGasEstimation}`;
         } else if(senderBalance < amountInWei) {
-            console.log(senderBalance, amountInWei, senderBalance < amountInWei);
+            console.log("Your token balance is too low : ", senderBalance, amountInWei, senderBalance < amountInWei);
             response.error.balance = "Your token balance is too low."
         } else if (!transactionApproved) {
             response.error.send = "Transaction is not approved";
